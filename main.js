8e4b15e2e66ba81bd2ecddd9e7180dc1aa21379c
TylerBerlin <tyberlin@cox.net>
2019-12-02 23:27:54 -0800

    [Disabled] Error logging

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ if (!fs.existsSync(configDir))
       return err;
   });
 
-log.transports.file.file = configDir + '/SR-Launcher-log.txt';
+/*log.transports.file.file = configDir + '/SR-Launcher-log.txt';
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 
 if (err !== undefined)
-  log.info(err);
+  log.info(err);*/
 
 let mainWindow;
 
